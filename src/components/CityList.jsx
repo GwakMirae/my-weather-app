433f954ff3de48204665d1b08934dc6ba96617a5
GwakMirae <alfo9490@naver.com>
2021-05-13 23:54:37 +0900

    modified cityList homework

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,15 +2,30 @@
 function CityList(props){
     // const cities = props.cities;
     const {cities} = props;
-    
+    var alpha = "A";
+    var prev_alpha = "A";
+    var list = "";
+    var temp = "";
     return (
         <div>
             <h1>CityList</h1>
             
-            <ul>
+            <ul className="cityList">
                 {cities.map((item, index) => {
-                    return <li key={index}>{item}</li>
+                    alpha = item.charAt(0).toUpperCase();
+                    
+                    if (prev_alpha === alpha){
+                        list = list + item +" / "
+                        prev_alpha = alpha  
+                    }
+                    else{
+                        temp = list;
+                        list = item + " / "
+                        prev_alpha = alpha
+                        return <li key={index}>{temp}</li> 
+                    }
                 })}
+                <li key={prev_alpha}>{list}</li>
             </ul>
         </div>
     )
